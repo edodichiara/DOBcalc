85a5488ddadc9accfb9327fc637d2faf64333b04
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-10-20 10:28:26 +0200

    Add DatePickerDialog

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dobcalc.databinding.ActivityMainBinding
+import org.threeten.bp.OffsetDateTime
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSelectDate.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+        val calendar = OffsetDateTime.now()
+        val year = calendar.year
+        val month = calendar.monthValue
+        val day = calendar.dayOfMonth
+        DatePickerDialog(
+            this,
+            { view, year, month, dayOfMonth -> },
+            year,
+            month,
+            day
+        ).show()
     }
 }
