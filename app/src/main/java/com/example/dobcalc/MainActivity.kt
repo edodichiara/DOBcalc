fd9a2d0e947231f1671883335ff1e49b6720c1f5
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-10-20 10:59:57 +0200

    Update the clickDatePicker

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.dobcalc
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.dobcalc.databinding.ActivityMainBinding
 import org.threeten.bp.OffsetDateTime
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.tvTitle4.visibility = View.INVISIBLE
         binding.btnSelectDate.setOnClickListener {
             clickDatePicker()
         }
@@ -27,7 +32,16 @@ class MainActivity : AppCompatActivity() {
         val day = calendar.dayOfMonth
         DatePickerDialog(
             this,
-            { view, year, month, dayOfMonth -> },
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                binding.tvTitle4.text =
+                    getString(R.string.tv_title_4, selectedDayOfMonth, selectedMonth, selectedYear)
+                binding.tvTitle4.visibility = View.VISIBLE
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ITALY)
+
+                val date = sdf.parse(binding.tvTitle4.text.toString())
+                Log.v("Main ACtivity", "$date")
+            },
             year,
             month,
             day
